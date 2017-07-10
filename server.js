2917fd4092c6da58cc8bd23ed16510aec3359c59
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-10 18:55:07 +0530

    Serve index.html

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+// app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
 })
 
-
 app.get('/', (req, res) => {
-  res.send('Hello World !!!')
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 })
