05a9d9ad10a548f8ae632ba9135381ede8fcc3b4
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-12 18:57:47 +0530

    Add condition to display chat component

--- public/components/Main.js
+++ public/components/Main.js
@@ -5,12 +5,19 @@ import DefaultChat from 'DefaultChat'
 import Chat from 'Chat'
 
 export default class Main extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      chatSelected: false
+    }
+  }
   render () {
+    let chatSelected = this.state.chatSelected
     return (
       <div className='row'>
         <div className='col m4 user'><User /></div>
-        {/* <div className='col m8 defaultChat'><DefaultChat /></div> */}
-        <div className='col m8 chat'><Chat /></div>
+        {chatSelected ? <div className='col m8 chat'><Chat /></div>
+        : <div className='col m8 defaultChat'><DefaultChat /></div>}
       </div>
     )
   }
