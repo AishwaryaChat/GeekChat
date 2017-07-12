5bb97b80cf917a6c7ad47be0cb062eb88f7b509a
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-12 17:23:23 +0530

    Add icon for searching user

--- public/components/users/Search.js
+++ public/components/users/Search.js
@@ -3,8 +3,9 @@ import React from 'react'
 const Search = () => {
   return (
     <div className='row search'>
-      <div className='col m12'>
-        <input placeholder='Search' id='search' type='text' className='validate' />
+      <div className='col m1'><i className='small material-icons' id='search-icon'>search</i></div>
+      <div className='col m11'>
+        <input placeholder='Search' id='search-input' type='text' className='validate' />
       </div>
     </div>
   )
