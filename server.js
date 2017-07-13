ac2f957269f68e623c4af4c2a315bb1ee0bff095
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-13 15:20:12 +0530

    Add route and logic for signup

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyParser = require('body-parser')
+const fs = require('fs')
 
 app.use(express.static(path.join(__dirname, 'public/build')))
 app.use(express.static(path.join(__dirname, 'public/assets')))
 app.use(express.static(path.join(__dirname, 'views')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
 })
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'))
+let users = JSON.parse(fs.readFileSync('./data.json', 'utf8'))
 
+const addUserToDB = (user) => {
+  users.push(user)
+  users = fs.writeFileSync('./data.json', JSON.stringify(users, null, 4), 'utf8')
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'))
+})
+
+app.post('/signup', (req, res) => {
+  addUserToDB(req.body)
+  res.redirect('/')
 })
