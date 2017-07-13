24357884e273b0c5cf94dc655070f222c2bec497
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-13 18:48:47 +0530

    Add code to maintain cookie

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const fs = require('fs')
+const uuid = require('node-uuid')
 
 app.use(express.static(path.join(__dirname, 'public/build')))
 app.use(express.static(path.join(__dirname, 'public/assets')))
 app.use(express.static(path.join(__dirname, 'views')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
@@ -21,11 +24,17 @@ const addUserToDB = (user) => {
   users = fs.writeFileSync('./data.json', JSON.stringify(users, null, 4), 'utf8')
 }
 
-const validateUser = (loginUser, res) => {
+const validateUser = (loginUser, req, res) => {
+  console.log('something')
   users.map(user => {
     if (user.emailAddress === loginUser.emailAddress) {
-      console.log('loginUser')
-      return res.redirect('/home')
+      let cookie = req.cookies.cookieName
+      if (cookie === undefined) {
+        cookie = uuid.v4()
+        res.cookie('cookieName', cookie, {maxAge: 900000, httpOnly: true})
+        console.log('created Cookie successfully', cookie)
+      }
+      res.redirect('/home')
     }
   })
 }
@@ -40,9 +49,10 @@ app.post('/signup', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-  validateUser(req.body, res)
+  validateUser(req.body, req, res)
 })
 
 app.get('/home', (req, res) => {
+  console.log('homeeeeeee', req.cookies.cookieName)
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
