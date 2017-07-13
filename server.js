45cd9c2b3ccdfcc90c44fda514c3ba8a02dac1dd
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-13 11:00:45 +0530

    Add login and signup page

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'public/build')))
 app.use(express.static(path.join(__dirname, 'public/assets')))
+app.use(express.static(path.join(__dirname, 'views')))
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
 })
 
 app.get('/', (req, res) => {
-  if (req.session) {
     res.sendFile(path.join(__dirname, 'views/index.html'))
-  }
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+
 })
