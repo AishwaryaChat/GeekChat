a399cf557ec2d07c3cd925f563214d8d474f8469
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-14 21:18:23 +0530

    Add different file for chatServer

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const session = require('express-session')({
   saveUninitialized: true
 })
 const server = require('http').createServer(app)
-const io = require('socket.io').listen(server)
-const sharedSession = require('express-socket.io-session')
+const chatServer = require('./lib/chatServer')
 
 app.use(express.static(path.join(__dirname, 'public/build')))
 app.use(express.static(path.join(__dirname, 'public/assets')))
@@ -23,15 +22,12 @@ app.use(cookieParser())
 app.use(session)
 app.use('/io-square', express.static(path.join(__dirname, '/node_modules/io-square-browser/lib')))
 
-io.use(sharedSession(session, {autoSave: true}))
-
 server.listen(3000, () => {
   console.log('listening on port 3000')
 })
 
 // let users = JSON.parse(fs.readFileSync('./data.json', 'utf8'))
 let users = require('./data.json')
-let onlineUsers = []
 
 const addUserToDB = (user) => {
   user.onlineFlag = false
@@ -91,39 +87,4 @@ app.get('/logout', (req, res) => {
   res.redirect('/')
 })
 
-io.sockets.on('connection', socket => {
-  socket.on('new user', () => {
-    users.map(user => {
-      if (user.emailAddress === socket.handshake.session.user_id) {
-        user.socket = socket.id
-        onlineUsers.push(user)
-      }
-    })
-    getOnlineUsers(socket)
-  })
-  socket.on('disconnect', (data) => {
-    users.map(user => {
-      if (user.socket === socket.id) {
-        user.onlineFlag = false
-      }
-    })
-    console.log('user disconnected')
-  })
-})
-
-const getOnlineUsers = (socket) => {
-  console.log('inside getOnlineUsers')
-  var i = 0
-  for (; i < onlineUsers.length; i++) {
-    let onlineUserList = []
-    onlineUsers.map(user => {
-      if (user.emailAddress !== onlineUsers[i].emailAddress) {
-        onlineUserList.push({
-          name: `${user.firstname} ${user.lastname}`,
-          userid: user.userid,
-          emailAddress: user.emailAddress})
-      }
-    })
-    io.sockets.in(onlineUsers[i].socket).emit('getOnlineUsers', onlineUserList)
-  }
-}
+chatServer.listen(server, session, users)
