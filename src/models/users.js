1cee9ca62ed80b77bf621456737c45afeedf6f7e
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-07-16 12:48:33 +0530

    Fix user schema

--- src/models/users.js
+++ src/models/users.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  id: String,
   name: String,
   firstname: String,
   lastname: String,
   emailAddress: String,
-  password: String,
-  onlineFlag: Boolean
+  password: String
 })
 
 const UsersSchema = mongoose.model('Users', userSchema)
